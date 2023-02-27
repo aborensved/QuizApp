f9c6034cf8b078001cf8c0e94a0ad11642387a7b
Arvid Borensved <arvid.borensved@boardsonfire.com>
2023-02-27 08:21:11 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+  import { RouterView } from "vue-router";
+  import QuizesView from "./views/QuizesView.vue";
+
+  // 5:55:40 - the Ultimate Vue 3 Tutorial (100% composition API)
+</script>
+
+<template>
+  <div class="container">
+    <RouterView />
+
+  </div>
+</template>
+
+<style scoped>
+    .container {
+    max-width: 1000px;
+    margin: 0 auto;
+  }
+</style>
